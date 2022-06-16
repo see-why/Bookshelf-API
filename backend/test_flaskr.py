082ad3cceff78fd8ace571540abff605564a1f56
see-why <iyadicy@gmail.com>
2022-06-16 22:52:10 +0100

    tests for new book endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -68,6 +68,23 @@ class BookTestCase(unittest.TestCase):
         self.assertEqual(data["success"], False)
         self.assertEqual(data["message"], "Bad request")
 
+    def test_create_new_book(self):
+        res = self.client().post("/books", json=self.new_book)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["created"])
+        self.assertTrue(len(data["books"]))
+
+    def test_405_if_book_creation_not_allowed(self):
+        res = self.client().post("/books/45", json=self.new_book)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "Method not allowed")
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
